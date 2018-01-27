a122528049655f06a326bd3f23bd57f26a46d303
LAPTOP-C1IPJMT2\TatuL <tatu@vaadin.com>
2018-01-27 20:16:47 +0200

    Version 1.0.1: Backporting fix to issue #2 from 2.0.1: Server side position was off sync after setPosition(..)

--- GridScrollExtension-addon/src/main/java/org/vaadin/extension/gridscroll/GridScrollExtension.java
+++ GridScrollExtension-addon/src/main/java/org/vaadin/extension/gridscroll/GridScrollExtension.java
@@ -4,7 +4,6 @@ import org.vaadin.extension.gridscroll.shared.GridScrollExtensionClientRPC;
 import org.vaadin.extension.gridscroll.shared.GridScrollExtensionServerRPC;
 
 import com.vaadin.server.AbstractExtension;
-import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Grid;
 
 // This is the server-side UI component that provides public API 
@@ -51,6 +50,8 @@ public class GridScrollExtension extends AbstractExtension {
 
 	public void setScrollPosition(int Xposition, int Yposition) {
 		getClientRPC().setScrollPosition(Xposition, Yposition);
+		lastXPosition = Xposition;
+		lastYPosition = Yposition;
 	}
 
 	private GridScrollExtensionClientRPC getClientRPC() {
